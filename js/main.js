5e5ca87fad5e514a91ec3b0556fd6413c438a922
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-06-13 11:01:28 +0300

    Adds forms submit javascript handler

--- js/main.js
+++ js/main.js
@@ -7,4 +7,10 @@
     const { height: headerHeight } = refs.header.getBoundingClientRect();
     document.body.style.paddingTop = `${headerHeight}px`;
   }
+
+  document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
+  });
 })();
